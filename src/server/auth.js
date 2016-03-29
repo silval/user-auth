d1e3c42a49dc7e28dd60a1da8ccae7828b2b6e54
Luciano Silva <lsilva@pivotal.io>
2016-03-29 16:14:56 -0400

    Refactoring 3

--- src/server/auth.js
+++ src/server/auth.js
@@ -1,12 +1,12 @@
 var jwt = require('jwt-simple');
 
-var auth = {
-
+function Auth() {
+}
   // curl -H "Content-Type: application/json" -X POST -d '{"username":"xyz","password":"xyz"}' http://127.0.0.1:8085/login
 
   // curl -H "Content-Type: application/json" -X GET -d '{"access_token":"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE0NTk3NTA2OTg4MDV9.5J2KlDURlFrFWzS0Cjokyk5JtGI1e5f7z6Iv90jgAOE","x_key":"arvind@myapp.com"}' http://127.0.0.1:8085/api/v1/listAnchorTypes
 
-  login: function(req, res) {
+Auth.prototype.login = function(req, res) {
     var username = req.body.username || '';
     var password = req.body.password || '';
     if (username == '' || password == '') {
@@ -33,33 +33,71 @@ var auth = {
     // authentication is successfull, then generate a token and dispatch it to the client
     res.json(genToken(dbUserObj,7,require('./secret')()));
 
-  },
-  validate: function(username, password) {
+};
+Auth.prototype.validateRequest= function(req, res, next) {
+
+  // When performing a cross domain request, you will recieve
+  // a preflighted request first. This is to check if our the app
+  // is safe.
+
+  var token = (req.body && req.body.access_token) || (req.query && req.query.access_token) || req.headers['x-access-token'];
+  var key = (req.body && req.body.x_key) || (req.query && req.query.x_key) || req.headers['x-key'];
+
+  if (token || key) {
+    try {
+      var decoded = jwt.decode(token, require('./secret.js')());
+
+      if (decoded.exp <= Date.now()) {
+        return returnFunction(res, 400, "TOKEN_EXPIRED");
+      }
+      if (decoded.username != key) {
+        // Token username does not match username in request, respond back with a 401
+        return returnFunction(res, 401, "INVALID_CREDENTIALS");
+      }
+
+      // Authorize the user to see if s/he can access our resources
+      var dbUser = this.authorizeUser(key, req.url); // The key would be the logged in user's username
+
+      if (dbUser && dbUser != null) {
+        next(); // To move to next middleware
+      } else {
+        return returnFunction(res, 403, "NOT_AUTHORIZED");
+      }
+
+    } catch (err) {
+      return returnFunction(res, 500, "TOKEN_DECODING_ERROR");
+    }
+  } else {
+    return returnFunction(res, 401, "INVALID_TOKEN_OR_KEY");
+  }
+};
+Auth.prototype.validate = function(username, password) {
     // spoofing the DB response for simplicity
     var dbUserObj = this.test_genDummyUserObj();
     return dbUserObj;
-  },
-  authorizeUser: function(username, url) {
+};
+
+Auth.prototype.authorizeUser = function(username, url) {
     // spoofing the DB response for simplicity
     var dbUserObj = null;
     if (username == "joe@doe.com") {
       dbUserObj = this.test_genDummyUserObj();
     }
     return dbUserObj;
-  },
-  test_genExpiredToken: function(userObj,secret) {
+};
+Auth.prototype.test_genExpiredToken = function(userObj,secret) {
     return genToken(userObj,0,secret);
-  },
-  test_genValidToken: function(userObj, secret) {
+};
+Auth.prototype.test_genValidToken = function(userObj, secret) {
     return genToken(userObj,1,secret);
-  },
-  test_genDummyInvalidUserObj: function() {
+};
+Auth.prototype.test_genDummyInvalidUserObj = function() {
     return { name: 'joebot', role: 'admin', username: 'joe@bot.com'};
-  },
-  test_genDummyUserObj: function() {
+};
+Auth.prototype.test_genDummyUserObj = function() {
     return { name: 'joedoe', role: 'admin', username: 'joe@doe.com'};
-  }
-}
+};
+
 // private methods
 function genToken(user, nbrOfDays, secret) {
   var expires = expiresIn(nbrOfDays); // 7 days
@@ -77,4 +115,13 @@ function expiresIn(numDays) {
   var dateObj = new Date();
   return dateObj.setDate(dateObj.getDate() + numDays);
 }
-module.exports = auth;
+function returnFunction(res, statusCode, message) {
+  var retObj= {
+    "status": statusCode,
+    "message": message
+  };
+  res.status(statusCode);
+  res.json(retObj);
+  return JSON.stringify(retObj);
+}
+module.exports = Auth;
--- tests/auth-tests.js
+++ tests/auth-tests.js
@@ -1,10 +1,11 @@
 var chai = require('chai');
 var expect = chai.expect
   , should = chai.should();
+var Auth = require(__dirname+'/../src/server/auth');
 
 describe('auth unit tests', function() {
 
-  var auth = require(__dirname+'/../src/server/auth.js');
+  var auth = new Auth();
 
   // login
   it('login() should return INVALID_CREDENTIALS error if no object ID is in request', function() {
@@ -16,5 +17,27 @@ describe('auth unit tests', function() {
     expect(auth.authorizeUser(auth.test_genDummyInvalidUserObj().username)).to.have.null;
   });
 
+  // validateRequest
+  it('validateRequest() should return INVALID_TOKEN_OR_KEY error if no token/key is in request', function() {
+    expect(auth.validateRequest({ "body" : {}, "headers": {} },{ "status" : function () {} , "json" : function() {}},function(msg){ return msg})).to.have.string("INVALID_TOKEN_OR_KEY");
+  });
+  it('validateRequest() should return TOKEN_EXPIRED error if no token/key is in request', function() {
+    var expiredToken = auth.test_genExpiredToken(auth.test_genDummyUserObj,require('./../src/server/secret')());
+    expect(auth.validateRequest({ "body" : { "access_token" : expiredToken.token, "x_key" : expiredToken.user.username }, "headers": {} },{ "status" : function () {} , "json" : function() {}},function(msg){ return msg})).to.have.string("TOKEN_EXPIRED");
+  });
+
+  it('validateRequest() should return TOKEN_DECODING_ERROR error if invalid token is in request', function() {
+    expect(auth.validateRequest({ "body" : { "access_token" : "1234", "x_key" : "bla" }, "headers": {} },{ "status" : function () {} , "json" : function() {}},function(msg){ return msg})).to.have.string("TOKEN_DECODING_ERROR");
+  });
+
+  it('validateRequest() should return INVALID_CREDENTIALS error if token userid does not match key', function() {
+    var token = auth.test_genValidToken(auth.test_genDummyInvalidUserObj(),require('./../src/server/secret')());
+    expect(auth.validateRequest({ "body" : { "access_token" : token.token, "x_key" : "bla" }, "headers": {} },{ "status" : function () {} , "json" : function() {}},function(msg){ return msg})).to.have.string("INVALID_CREDENTIALS");
+  });
+
+  it('validateRequest() should return NOT_AUTHORIZED error if user is not authorized', function() {
+    var token = auth.test_genValidToken(auth.test_genDummyInvalidUserObj(),require('./../src/server/secret')());
+    expect(auth.validateRequest({ "body" : { "access_token" : token.token, "x_key" : token.user.username }, "headers": {} },{ "status" : function () {} , "json" : function() {}},function(msg){ return msg})).to.have.string("NOT_AUTHORIZED");
+  });
 
 });
